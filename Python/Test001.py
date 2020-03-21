1b15be1a5344a102a02186d80a84c6c46a7840b2
Micah <sep85911@outlook.com>
2020-03-22 00:56:16 +0800

    202003220056

--- Python/Test001.py
+++ Python/Test001.py
@@ -1,9 +1,21 @@
 import os;
 import sys;
+import re;
 
 import ToolFunction;
 
 if __name__ == "__main__":
     obj001 = ToolFunction.CCreature();
+
+
+    sWord = "abcdef234gefafdffdfegsdggrgrgfdfafefefefdsss";
+
+    sPattern = "[A-Za-z]+";
+    style001 = re.compile(sPattern);
+    a = style001.match(sWord);
+
+    a.group(0);
+
+
 
     print(obj001.ID);
