ecc96f8844855360211f87258b4b44772b928309
Micah <sep85911@outlook.com>
2020-04-04 23:20:57 +0800

    202004042320

---
+++ Python/Calendar.py
@@ -0,0 +1,10 @@
+import calendar;
+
+
+if __name__ == "__main__":
+    
+    print(calendar.isleap(2000)); # 判断是否为闰年
+
+    print(calendar.weekday(2020,4,6));  # 返回星期几 0是星期一 6是星期日
+
+    
--- Python/IO.py
+++ Python/IO.py
@@ -6,9 +6,6 @@ import os;
 import shutil;
 
 if __name__ == "__main__":
-    
-
-    shutil.copy()
     # open函数 参数为：路径 读写模式 编码格式
     with codecs.open('./Python/ToolFunction.py','rb',encoding='utf-8') as f:
         lines = f.readlines();
