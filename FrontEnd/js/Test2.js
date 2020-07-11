ca41905a5b5ad568f002545af038cfd3422ae1cf
Micah <sep85911@outlook.com>
2020-07-11 11:10:39 +0800

    202007111110

--- FrontEnd/js/Test2.js
+++ FrontEnd/js/Test2.js
@@ -13,3 +13,19 @@ for(var i = 0; i < btns.length; i ++)
         this.className = "active";
     }
 }
+
+var div1 = document.getElementById('cube');
+
+// 不能直接操作css 只能通过html里面的style间接操作css
+div1.style.width = '100px';
+div1.style.height = '100px';
+div1.style.backgroundColor = "lightcoral";
+div1.style.position = 'absolute';
+div1.style.left = '100px';
+div1.style.top = '123px';
+
+
+//平移小方块
+setInterval(function (){
+   div1.style.left = parseInt( div1.style.left ) + 1 + 'px'
+},50)
