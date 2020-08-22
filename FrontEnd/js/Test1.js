edb8d2b2954bcbcacf316ef296b948aa1dc1b248
Micah <sep85911@outlook.com>
2020-08-22 12:47:57 +0800

    202008221247

--- FrontEnd/js/Test1.js
+++ FrontEnd/js/Test1.js
@@ -1,19 +1,6 @@
-var a = document.getElementsByClassName('test1')[0];
+var mylist = new Array()
+mylist[1] = "123";
+mylist[2] = 123;
 
-a.style.color = "#321"
-a.style.textDecoration = 'None';
-a.style.backgroundColor = "pink";
-
-var bSwitch = true;
-a.onclick = function(){
-    
-    if(bSwitch)
-    {
-        this.style.backgroundColor = "blue"
-        bSwitch = false;
-    }else
-    {
-        this.style.backgroundColor = "pink"
-        bSwitch = true;
-    }
-}
+console.log("我就呵呵了" + typeof(mylist))
+console.log(mylist)
