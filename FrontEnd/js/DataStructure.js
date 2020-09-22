15b48c53883bf9b4206d180aaa2391c24585e83b
Micah <sep85911@outlook.com>
2020-09-22 22:21:30 +0800

    202009222221

--- FrontEnd/js/DataStructure.js
+++ FrontEnd/js/DataStructure.js
@@ -23,3 +23,27 @@ console.log(concated);
 //查找数组中第一个符合的元素 返回其索引 没有的话 返回-1
 var indof = list002.indexOf("fucked")
 console.log(indof);
+
+// every函数 参数是一个匿名函数 依次传入数组中每一个元素 执行匿名函数后有一个为false 则every返回false 否则返回true
+var everyfunc = list002.every(function(a){
+    return a != undefined;
+})
+console.log("everyfunc:" + everyfunc)
+
+// some函数 参数是一个匿名函数 依次传入数组中每一个元素 执行匿名函数后返回一个true 则为true 否则为false
+var somefunc = list002.some(function(a){
+    return a > 54653;
+})
+console.log("somefunc:" + somefunc)
+
+// foreach函数 依次传入数组中的每一个元素 然后依次执行
+list002.forEach(function(a){
+    console.log(a + '  forEach');
+})
+
+//filter函数 用于过滤 参数是一个匿名函数 依次传入数组中每一个元素 执行匿名函数后返回true的会加入一个数组并作为返回值返回 false的不会
+var filtered = list002.filter(function(element){
+    return element > 1000;
+})
+
+console.log(filtered)
